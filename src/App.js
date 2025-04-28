fa0ff7b3b2bf160a877ba5cf125a1eee983fea78
nikitaax <nikkitanandankar@gmail.com>
2025-04-28 13:13:21 +0530

    props and rating

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import './styles.css';
 import Header from './Components/Header';
 import Footer from './Components/Footer';
+import MovieGrid from './Components/MovieGrid';
 
 function App() {
   return (
     <div className="App">
       <div className='container'>
         <Header></Header>
+        <MovieGrid></MovieGrid>
       </div>
       <Footer></Footer>
     </div>
